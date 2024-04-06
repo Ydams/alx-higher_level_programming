63add84113d3cb6de324177419b3fcb33909f26f
Ydams <Damola4542@gmail.com>
2024-04-06 09:38:13 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z')+1):
-    print(chr(i), end='') # This function ensures that the characters are printed without a newline.
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='') # This function ensures that the characters are printed without a newline.
