f2d8f88e61cebf4ae9fdeb8ae7c92c8e46d18cc3
Ydams <Damola4542@gmail.com>
2024-04-06 10:11:09 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # This function creates a copy of the string, removing the character at the posnt
-def remove_char_at(str, n):
+
+def remove_char_at(input_str, n):
     if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+        return input_str
+    return input_str[:n] + input_str[n+1:]
+
+
