c3f5daaffeffdee67a51adf7d16fd2b1e879a4be
Ydams <Damola4542@gmail.com>
2024-04-06 09:46:11 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #This is a code that prints all possible different combinations of two digits
-    for x in range(0, 10):
+ for x in range(0, 10):
     for y in range(x + 1, 10):
         if x == 0 and y == 9:
             print('89')
