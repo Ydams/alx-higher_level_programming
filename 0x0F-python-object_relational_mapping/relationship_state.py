486ed5d0cd1d8e11bc8286134b8f5fb1b5543f52
Ydams <Damola4542@gmail.com>
2024-04-16 04:30:29 -0700

    python Sqlachemy

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script defines a State class and
-a Base class to work with MySQLAlchemy ORM respectively
+The script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
+
 """
 
 from sqlalchemy import Column, Integer, String
